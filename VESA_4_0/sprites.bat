bf9750bdf605b450352fc60e1f31d4f57295cb49
nedo-coder <roshin12slava12@mail.ru>
2020-09-21 13:19:30 +0300

    A

--- VESA_4_0/sprites.bat
+++
@@ -1,4 +0,0 @@
-del allspr.dat
-copy C:\DosBox\Soft\VESA_4_0\tanktextures\trava.spr allspr.dat
-copy /b allspr.dat+C:\DosBox\Soft\VESA_4_0\tanktextures\blockwall.spr+C:\DosBox\Soft\VESA_4_0\tanktextures\m1\r.spr+C:\DosBox\Soft\VESA_4_0\tanktextures\m1\l.spr+C:\DosBox\Soft\VESA_4_0\tanktextures\m1\u.spr+C:\DosBox\Soft\VESA_4_0\tanktextures\m1\d.spr
-copy /b allspr.dat+C:\DosBox\Soft\VESA_4_0\tanktextures\t-14\r.spr+C:\DosBox\Soft\VESA_4_0\tanktextures\t-14\l.spr+C:\DosBox\Soft\VESA_4_0\tanktextures\t-14\u.spr+C:\DosBox\Soft\VESA_4_0\tanktextures\t-14\d.spr
